216a788766783cf3cd6ea8e55d17ad8e3d51db16
Thiago Leite <thiago.leite@idealeware.com.br>
2017-11-26 19:42:52 -0200

    Persistências

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -6,7 +6,9 @@ module.exports = function (app) {
 
     app.post('/pagamentos/pagamento', function (req, res) {
         var pagamento = req.body;
-        console.log(pagamento);
-        res.send('OK');
+        console.log('Processando uma requisição de um novo pagamento');
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date();
+        res.send(pagamento);
     });
 }
