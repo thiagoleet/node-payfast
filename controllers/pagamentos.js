60a087c18fede993c0ed7f1d4a93133a9be0edcd
Thiago Leite <thiago.leite@idealeware.com.br>
2017-11-26 19:21:54 -0200

    CustomExpress, BodyParser

---
+++ controllers/pagamentos.js
@@ -0,0 +1,12 @@
+module.exports = function (app) {
+    app.get('/pagamentos', function (req, res) {
+        console.log('Recebida requisição de teste na porta 3000');
+        res.send('OK');
+    });
+
+    app.post('/pagamentos/pagamento', function (req, res) {
+        var pagamento = req.body;
+        console.log(pagamento);
+        res.send('OK');
+    });
+}
--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-var express = require('express');
-var app = express();
+var app = require('./config/custom-express')();
 
 app.listen(3000, function() {
     console.log('Servidor rodando na porta 3000');
-});
-
-app.get('/teste', function(req, res) {
-    console.log('Recebida requisição de teste na porta 3000');
-    res.send('OK');
 });
