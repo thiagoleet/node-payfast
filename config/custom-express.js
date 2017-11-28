d9c5010a507cb5561033994c4d6f748a66f42ec3
thiagofmleite <thiagofmleite@gmail.com>
2017-11-28 14:14:51 -0200

    Persistências no banco mysql

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var validator = require('express-validator');
 
 module.exports = function () {
     var app = express();
 
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+    app.use(validator());
 
     consign()
         .include('controllers')
+        .then('persistences')
         .into(app);
 
     return app;
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,14 +1,47 @@
 module.exports = function (app) {
     app.get('/pagamentos', function (req, res) {
         console.log('Recebida requisição de teste na porta 3000');
-        res.send('OK');
+        console.log('Listando todos os pagamentos');
+        var connection = app.persistences.connectionFactory();
+        var dao = new app.persistences.PagamentoDao(connection);
+        dao.lista(function (error, result) {
+            console.log('Consulta realizada');
+            res.json(pagamento);
+        });
     });
 
     app.post('/pagamentos/pagamento', function (req, res) {
+        // Validações
+        req.assert("forma_de_pagamento", "Forma de pagamento é obrigatório")
+            .notEmpty();
+        req.assert("valor", "Valor é obrigatório e deve ser um decimal")
+            .notEmpty()
+            .isFloat();
+        
+        var erros = req.validationErrors();
+        if(erros) {
+            console.log('Erros de validação encontrados');
+            res.status(400).send(erros);
+            return;
+        }
+
         var pagamento = req.body;
         console.log('Processando uma requisição de um novo pagamento');
         pagamento.status = 'CRIADO';
         pagamento.data = new Date();
-        res.send(pagamento);
+
+        var connection = app.persistences.connectionFactory();
+        var dao = new app.persistences.PagamentoDao(connection);
+        dao.salva(pagamento, function (error, result) {
+            if (error) {
+                console.error(error);
+                res.status(400).json('Erro ao criar o pagamento');
+            }
+            else {
+                console.log('Pagamento criado');
+                res.json(pagamento);
+            }
+        });
+
     });
 }
